d9c90a2cc698714bcd4e6297882f3adc1b1c1f49
Yuri Kozyk <y.kozyk@gmail.com>
2017-01-18 21:51:24 +0200

    20170118H

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
 $(function() {
 
-	// Custom JS
+	$(document).ready(function(){
+		$(".owl-carousel").owlCarousel({
+
+			loop: true,
+			margin: 30,
+			nav: true,
+			navText: [
+			'<i class="fa fa-chevron-left fa-border" aria-hidden="true"></i>',
+			'<i class="fa fa-chevron-right fa-border" aria-hidden="true"></i>'
+			]
+
+			});
+	});
 
 });

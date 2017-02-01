b01e7775563188c60babe3a87d2e24630db9e81e
Yuri Kozyk (work) <y.kozyk@gmail.com>
2017-02-01 16:19:08 +0200

    add mobile menu (jQuery)

--- app/js/common.js
+++ app/js/common.js
@@ -1,18 +1,33 @@
-$(function() {
+$(document).ready(function(){
 
-	$(document).ready(function(){
-		$(".owl-carousel").owlCarousel({
+	$(".owl-carousel").owlCarousel({
 
-			items: 3,
-			loop: true,
-			margin: 30,
-			nav: true,
-			navText: [
-			'<i class="fa fa-chevron-left fa-border" aria-hidden="true"></i>',
-			'<i class="fa fa-chevron-right fa-border" aria-hidden="true"></i>'
-			]
+		items: 3,
+		loop: true,
+		margin: 30,
+		nav: true,
+		navText: [
+		'<i class="fa fa-chevron-left fa-border" aria-hidden="true"></i>',
+		'<i class="fa fa-chevron-right fa-border" aria-hidden="true"></i>'
+		]
 
-			});
 	});
 
+	/* Mobile munu */
+
+	$(".toggle-menu, .mobile-menu li, wrapper-invis").click(function() {
+		$(".burger").toggleClass("active");
+		$(".mobile-menu").slideToggle();
+	});
+
+	$(document).click(function(e){
+
+   if($(e.target).closest('.mobile-menu-wrapper').length != 0) return false;
+  	$(".burger").removeClass("active");
+  	$(".mobile-menu").hide();
+	});
+
+
+
 });
+
